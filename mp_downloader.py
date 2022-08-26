983fafbaed483db4927f75d7465e5cd28db17e92
Rio <riobeggs1@gmail.com>
2022-08-26 23:41:09 +1200

    got flask set up

--- mp_downloader.py
+++ mp_downloader.py
@@ -1,6 +1,22 @@
-from 
+from pytube import YouTube
+from flask import Flask, render_template, request
 
-def get_url() -> str:
-    url = "test"
 
-    
+app = Flask(__name__)
+
+
+@app.route("/", methods=("GET", "POST"))
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+
+    if request.method == "POST":
+        try:
+            print("success")
+        except:
+            failed = True
+            return render_template("home.html", failed=failed)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
